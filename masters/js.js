367ca810911513b190dd836225fb325ad43000dd
SobinDA <SobinDA@srgroup.ru>
2023-06-16 17:37:07 +0300

    Добавлен код. Происходит отрисовка флага с данными

--- masters/js.js
+++ masters/js.js
@@ -1,3 +1,6 @@
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
 const request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.com/v3.1/name/russia');
 request.send();
@@ -6,16 +9,18 @@ request.addEventListener('load', function () {
     console.log(data);
 
 
-const html = `
+    const html = `
 <article class="country">
-    <img class="country_img" src="${data.flags.svg}">
+    <img class="country_img" src="${data.flags.svg}"/>
     <div class="country data">
         <h3 class="country_name">"${data.name.common}"</h3>
         <h4 class="country_region">${data.region}</h4>
-        <p class="country_row"><span>👪</span> ${data.population}</p>
-        <p class="country_row"><span>👤</span> ${data.languages}</p>
-        <p class="country_row"><span>💰</span> ${data.currencise.symbol}</p>
+        <p class="country_row"><span>👪</span> ${(+data.population/1000000).toFixed(1)} миллионов</p>
+        <p class="country_row"><span>👤</span> ${data.languages.rus}</p>
+        <p class="country_row"><span>💰</span> ${data.currencies.RUB.symbol}</p>
     </div>
 </article>
-`
+`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
 });
