5bfee8a0c7caca68a188fbe4e64dc7a1865a0a9e
SobinDA <SobinDA@srgroup.ru>
2023-06-19 12:53:24 +0300

    Готово - Необходимо добавить отступы между иконками

--- masters/js.js
+++ masters/js.js
@@ -1,26 +1,40 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/russia');
-request.send();
-request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.response)
-    console.log(data);
+const getCountryData = function (countryName) {
 
 
-    const html = `
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${countryName}`);
+    request.send();
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.response)
+        console.log(data);
+
+        const currencies = data.currencies;
+        const currencyName = Object.values(currencies)[0].name;
+        const currencySymbol = Object.values(currencies)[0].symbol;
+
+        const languages = data.languages;
+        const languageCountry = Object.values(languages)[0].valueOf();
+
+        const html = `
 <article class="country">
     <img class="country_img" src="${data.flags.svg}"/>
     <div class="country data">
         <h3 class="country_name">"${data.name.common}"</h3>
         <h4 class="country_region">${data.region}</h4>
-        <p class="country_row"><span>👪</span> ${(+data.population/1000000).toFixed(1)} миллионов</p>
-        <p class="country_row"><span>👤</span> ${data.languages.rus}</p>
-        <p class="country_row"><span>💰</span> ${data.currencies.RUB.symbol}</p>
+        <p class="country_row"><span>👪</span> ${(+data.population / 1000000).toFixed(1)} миллионов</p>
+        <p class="country_row"><span>👤</span> ${languageCountry}</p>
+        <p class="country_row"><span>💰</span> ${currencyName} : ${currencySymbol}</p>
     </div>
 </article>
 `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-});
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+    });
+}
+getCountryData('russia');
+getCountryData('usa');
+getCountryData('canada');
+getCountryData('bel');
